8c47f0de8aea5352ae386a53c2485822a723c90e
Manasi <manasitilak12@gmail.com>
2023-10-30 22:36:50 +0530

    Added Butterfly Pattern, Cross Pattern, Spiral Pattern

---
+++ 14.spiral_pattern.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int matrix[n][n];
+
+    int num = 1;
+    int startRow = 0, endRow = n - 1, startCol = 0, endCol = n - 1;
+
+    while (startRow <= endRow && startCol <= endCol) {
+        for (int i = startCol; i <= endCol; ++i) {
+            matrix[startRow][i] = num++;
+        }
+        ++startRow;
+
+        for (int i = startRow; i <= endRow; ++i) {
+            matrix[i][endCol] = num++;
+        }
+        --endCol;
+
+        if (startRow <= endRow) {
+            for (int i = endCol; i >= startCol; --i) {
+                matrix[endRow][i] = num++;
+            }
+            --endRow;
+        }
+
+        if (startCol <= endCol) {
+            for (int i = endRow; i >= startRow; --i) {
+                matrix[i][startCol] = num++;
+            }
+            ++startCol;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
